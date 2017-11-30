721d04e00085b483d1da1515be7f78da86f1ee97
Rodica Trifan <trodicaro@gmail.com>
2017-11-30 15:19:59 -0600

    Scaffold testing file

---
+++ solution_test.py
@@ -0,0 +1,12 @@
+import unittest
+from solution import find_buckets
+
+
+class Buckets(unittest.TestCase):
+    def first_test(self):
+        "Finds first bucket?"
+        self.assertTrue(true)
+
+
+if __name__ == '__main__':
+    unittest.main()
