89b8d9ba83599d1a75577e5689bdeacd9a077f8e
Rodica Trifan <trodicaro@gmail.com>
2017-12-05 14:24:20 -0600

    That's what I'd like to test, but don't know how yet :)

--- solution_test.py
+++ solution_test.py
@@ -1,7 +1,19 @@
 import unittest
 from solution import find_buckets
-
-
+# test if bucket is formed in the json
+# test for a very specific purchase record that it gets assigned to the specific bucket
+  # eg: 99680,8193774926972,PEARSON,SNA,7,10_day,2017-07-10 07:07:11.587228 => "Pearson,7,10_day"
+  # edge case when name includes dash
+  # edge case when name includes space
+  # edge case when name has lower and upper
+  # edge case repeated bucket
+# test for duration specific:
+  # eg: 98835,6544295182149,MACMILLAN,CLE,4,40_day,2017-01-10 14:08:55.562501 => "Macmillan,*,40_day"
+# test for price specific:
+  # eg: 98795,9277080469051,MCGRAW-HILL,MSP,6,120_day,2017-04-02 11:05:31.561470 => "McGraw-Hill,6,*"
+# test for only publisher specific:
+  # =>
+# test for catch-all bucket
 class Buckets(unittest.TestCase):
     def first_test(self):
         "Finds first bucket?"
