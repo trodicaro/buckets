1890b6142146153a18bfc267df39107bad39566b
Rodica Trifan <trodicaro@gmail.com>
2017-12-05 15:21:30 -0600

    Create the *,*,* bucket if it doesn't exist and move to top

--- solution.py
+++ solution.py
@@ -17,55 +17,59 @@ import csv
 import json
 from collections import OrderedDict
 
-buckets = OrderedDict()
+def sort_buckets():
+    buckets = OrderedDict()
+    # create a buckets dictionary; keep original key as first item in values
+    with open('min_buckets.csv') as buckets_file:
+        readCSV = csv.reader(buckets_file)
+        for row in readCSV:
+            original_key = ",".join([row[0],row[1],row[2]])
+            upcased_key = original_key.upper()
+            buckets[upcased_key] = [original_key]
 
-# create a buckets dictionary; keep original key as first item in values
-with open('purchase_buckets.csv') as buckets_file:
-    readCSV = csv.reader(buckets_file)
-    for row in readCSV:
-        original_key = ",".join([row[0],row[1],row[2]])
-        upcased_key = original_key.upper()
-        buckets[upcased_key] = [original_key]
+    if "*,*,*" not in buckets:
+        buckets["*,*,*"] = ["*,*,*"]
+        buckets.move_to_end("*,*,*", last=False)
 
-# iterate through purchases, build key and find match in buckets
-with open('purchase_data.csv') as purchases_file:
-    readCSV = csv.reader(purchases_file)
+    # iterate through purchases, build key and find match in buckets
+    with open('min_purchases.csv') as purchases_file:
+        readCSV = csv.reader(purchases_file)
 
-    for row in readCSV:
-        order_id = row[0]
-        publisher = row[2]
-        price = row[4]
-        duration = row[5]
-        key = ",".join([publisher, price, duration])
+        for row in readCSV:
+            order_id = row[0]
+            publisher = row[2]
+            price = row[4]
+            duration = row[5]
+            key = ",".join([publisher, price, duration])
 
-        complete_key = ",".join([publisher, price, duration]).upper()
-        publisher_duration_key = ",".join([publisher, "*", duration]).upper()
-        publisher_price_key = ",".join([publisher, price, "*"]).upper()
-        publisher_only = ",".join([publisher, "*", "*"]).upper()
-        price_duration_key = ",".join(["*", price, duration]).upper()
-        duration_only_key = ",".join(["*", "*", duration]).upper()
-        price_only_key = ",".join(["*", price, "*"]).upper()
-        catch_all_key = ",".join(["*","*","*"]).upper()
+            complete_key = ",".join([publisher, price, duration]).upper()
+            publisher_duration_key = ",".join([publisher, "*", duration]).upper()
+            publisher_price_key = ",".join([publisher, price, "*"]).upper()
+            publisher_only = ",".join([publisher, "*", "*"]).upper()
+            price_duration_key = ",".join(["*", price, duration]).upper()
+            duration_only_key = ",".join(["*", "*", duration]).upper()
+            price_only_key = ",".join(["*", price, "*"]).upper()
+            catch_all_key = ",".join(["*","*","*"]).upper()
 
-        key_list = [complete_key, publisher_duration_key, publisher_price_key, publisher_only,
-                    price_duration_key, duration_only_key, price_only_key, catch_all_key]
+            key_list = [complete_key, publisher_duration_key, publisher_price_key, publisher_only,
+                        price_duration_key, duration_only_key, price_only_key, catch_all_key]
 
-        for key in key_list:
-            if key in buckets:
-                stringified_record = ",".join(map(str, row))
-                buckets[key].append(stringified_record)
-                break
+            for key in key_list:
+                if key in buckets:
+                    stringified_record = ",".join(map(str, row))
+                    buckets[key].append(stringified_record)
+                    break
 
-results = []
+    results = []
 
-for bucket, content in buckets.items():
-    current_group = {}
-    current_group["bucket"] = content[0]
-    current_group["purchases"] = content[1:]
-    results.append(current_group)
+    for bucket, content in buckets.items():
+        current_group = {}
+        current_group["bucket"] = content[0]
+        current_group["purchases"] = content[1:]
+        results.append(current_group)
 
-results_file = open('results.json', 'w')
-results_file.write(json.dumps(results, indent = 4, sort_keys = True))
-results_file.close()
+    results_file = open('min_results.json', 'w')
+    results_file.write(json.dumps(results, indent = 4, sort_keys = True))
+    results_file.close()
 
 
