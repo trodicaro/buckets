20d2d5a04af43057cb5abe4939d0383b0f245324
Rodica Trifan <trodicaro@gmail.com>
2017-12-06 06:43:52 -0600

    Test for checking results file pass

--- solution_test.py
+++ solution_test.py
@@ -1,6 +1,7 @@
 import unittest
-from solution import find_buckets
-# test if bucket is formed in the json
+import os.path
+from solution import sort_buckets
+
 # test for a very specific purchase record that it gets assigned to the specific bucket
   # eg: 99680,8193774926972,PEARSON,SNA,7,10_day,2017-07-10 07:07:11.587228 => "Pearson,7,10_day"
   # edge case when name includes dash
@@ -14,11 +15,18 @@ from solution import find_buckets
 # test for only publisher specific:
   # =>
 # test for catch-all bucket
-class Buckets(unittest.TestCase):
-    def first_test(self):
-        "Finds first bucket?"
-        self.assertTrue(true)
 
+class BucketTest(unittest.TestCase):
+
+    def test_results_file_creation(self):
+        "Tests that a result file is generated"
+        self.assertTrue(os.path.isfile(TESTDATA_FILENAME))
+
+    # def test_keys(self):
+    #     "Test that generic bucket was created"
+    #     self.assertIn("*,*,*", results_file)
 
 if __name__ == '__main__':
+    sort_buckets('min_buckets.csv', 'min_purchases.csv')
+    TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'min_results.json')
     unittest.main()
