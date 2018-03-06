2959b00fb3de66b8534777789f9167157bc0242b
Rodica Trifan <trodicaro@gmail.com>
2018-03-06 09:47:38 -0600

    Tweak the specificy order; move the populating of the buckets in init method

--- bucket_collection.py
+++ bucket_collection.py
@@ -6,56 +6,23 @@ class Bucket:
     # https://pythonconquerstheuniverse.wordpress.com/2012/02/15/mutable-default-arguments/
     def __init__(self, original_key = ""):
         self.original_key = original_key
-        self.key = original_key.upper()
         self.purchases = []
 
 class BucketCollection:
-    def __init__(self, buckets_file_name):
+    def __init__(self, buckets_file_name, purchases_file_name):
         self.buckets = {}
-        self.all_keys = []
 
         with open(buckets_file_name) as buckets_file:
             readCSV = csv.reader(buckets_file)
-            for row in readCSV:
-                self.all_keys.append(",".join([row[0],row[1],row[2]]))
-                # bucket = Bucket(original_key)
-                # self.buckets[original_key.upper()] = bucket
 
-        if "*,*,*" not in self.all_keys:
-            self.all_keys.insert(0, "*,*,*")
-
-        for key in self.all_keys:
-            bucket = Bucket(key)
-            self.buckets[bucket.original_key.upper()] = bucket
-
-    def populate_buckets(self, purchases_file_name):
-        with open(purchases_file_name) as purchases_file:
-            readCSV = csv.reader(purchases_file)
+            self.buckets['*,*,*'] = Bucket('*,*,*')
 
             for row in readCSV:
-                order_id = row[0]
-                publisher = row[2]
-                price = row[4]
-                duration = row[5]
-                key = ",".join([publisher, price, duration])
+                original_key = ",".join([row[0],row[1],row[2]])
+                bucket = Bucket(original_key)
+                self.buckets[original_key.upper()] = bucket
 
-                complete_key = ",".join([publisher, price, duration]).upper()
-                publisher_duration_key = ",".join([publisher, "*", duration]).upper()
-                publisher_price_key = ",".join([publisher, price, "*"]).upper()
-                publisher_only = ",".join([publisher, "*", "*"]).upper()
-                price_duration_key = ",".join(["*", price, duration]).upper()
-                duration_only_key = ",".join(["*", "*", duration]).upper()
-                price_only_key = ",".join(["*", price, "*"]).upper()
-                catch_all_key = ",".join(["*","*","*"]).upper()
-
-                possible_keys = [complete_key, publisher_duration_key, publisher_price_key, publisher_only,
-                            price_duration_key, duration_only_key, price_only_key, catch_all_key]
-
-                for possible_key in possible_keys:
-                    if possible_key in self.buckets.keys():
-                        stringified_record = ",".join(map(str, row))
-                        self.buckets[possible_key].purchases.append(stringified_record)
-                        break
+        self.populate_buckets(purchases_file_name)
 
     def to_json(self):
         results = []
@@ -72,3 +39,34 @@ class BucketCollection:
       results_file = open(result_file_name, 'w')
       results_file.write(json.dumps(self.to_json(), indent = 4, sort_keys = True))
       results_file.close()
+
+    # make private
+    def populate_buckets(self, purchases_file_name):
+        with open(purchases_file_name) as purchases_file:
+            readCSV = csv.reader(purchases_file)
+
+            for row in readCSV:
+                order_id = row[0]
+                publisher = row[2]
+                price = row[4]
+                duration = row[5]
+                key = ",".join([publisher, price, duration])
+
+                complete_key = ",".join([publisher, price, duration]).upper()
+                publisher_duration_key = ",".join([publisher, "*", duration]).upper()
+                publisher_price_key = ",".join([publisher, price, "*"]).upper()
+                price_duration_key = ",".join(["*", price, duration]).upper()
+                publisher_only = ",".join([publisher, "*", "*"]).upper()
+                duration_only_key = ",".join(["*", "*", duration]).upper()
+                price_only_key = ",".join(["*", price, "*"]).upper()
+                catch_all_key = ",".join(["*","*","*"]).upper()
+
+                possible_keys = [complete_key, publisher_duration_key, publisher_price_key, price_duration_key, publisher_only, duration_only_key, price_only_key, catch_all_key]
+
+                for possible_key in possible_keys:
+                    #I don't think following line is efficient; alternatives?
+                    if possible_key in self.buckets.keys():
+                        stringified_record = ",".join(map(str, row))
+                        self.buckets[possible_key].purchases.append(stringified_record)
+                        break
+
