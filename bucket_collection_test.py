7503106afa8d12a6646b29da5513f2c2a509aef0
Rodica Trifan <trodicaro@gmail.com>
2018-03-07 14:26:55 -0600

    Most tests pass; just realized script doesn't create duplicate buckets

--- bucket_collection_test.py
+++ bucket_collection_test.py
@@ -51,6 +51,7 @@ class BucketCollectionTest(unittest.TestCase):
         self.assertTrue(next(keys_iterator) == "*,*,*")
         # check first bucket in file provided is next key
         self.assertTrue(next(keys_iterator) == "Pearson,*,*")
+        pass
 
     @unittest.skip("pending")
     def test_a_purchase_is_found_only_once(self):
@@ -62,61 +63,50 @@ class BucketCollectionTest(unittest.TestCase):
         pass
         # in a specific bucket check that the order_id's are going up
 
-    @unittest.skip("pending")
     def test_publisher_price_duration_bucket(self):
-        pass
-        # "Most specific"
-        # eg: 99680,8193774926972,PEARSON,SNA,7,10_day,2017-07-10 07:07:11.587228 => "Pearson,7,10_day"
+        "Most specific"
+        test_string = "99680,8193774926972,PEARSON,SNA,7,10_day,2017-07-10 07:07:11.587228"
+        self.assertIn(test_string, self.data_dictionary["Pearson,7,10_day"])
 
-    @unittest.skip("pending")
     def test_publisher_duration_bucket(self):
-        pass
-        # eg: 98835,6544295182149,MACMILLAN,CLE,4,40_day,2017-01-10 14:08:55.562501 => "Macmillan,*,40_day"
+        test_string = "98835,6544295182149,MACMILLAN,CLE,4,40_day,2017-01-10 14:08:55.562501"
+        self.assertIn(test_string, self.data_dictionary["Macmillan,*,40_day"])
 
-    @unittest.skip("pending")
     def test_publisher_price_bucket(self):
-        pass
-       # eg: 98795,9277080469051,MCGRAW-HILL,MSP,6,120_day,2017-04-02 11:05:31.561470 =>   "McGraw-Hill,6,*"
+        test_string = "98795,9277080469051,MCGRAW-HILL,MSP,6,120_day,2017-04-02 11:05:31.561470"
+        self.assertIn(test_string, self.data_dictionary["McGraw-Hill,6,*"])
 
-    @unittest.skip("pending")
     def test_price_duration_bucket(self):
-        pass
-        # eg: 98819,9793386372887,PENGUIN RANDOMHOUSE,DTW,3,90_day,2017-07-14 14:06:01.562089 => "*,3,90_day"
+        test_string = "98819,9793386372887,PENGUIN RANDOMHOUSE,DTW,3,90_day,2017-07-14 14:06:01.562089"
+        self.assertIn(test_string, self.data_dictionary["*,3,90_day"])
 
-    @unittest.skip("pending")
     def test_publisher_only_bucket(self):
-        pass
-        # eg: 98771,1899596499745,PEARSON,MIA,3,110_day,2017-05-23 09:16:43.560846 => "Pearson,*,*"
+        test_string = "98771,1899596499745,PEARSON,MIA,3,110_day,2017-05-23 09:16:43.560846"
+        self.assertIn(test_string, self.data_dictionary["Pearson,*,*"])
 
-    @unittest.skip("pending")
     def test_publisher_only_bucket_with_upper_lower_letters(self):
-        pass
-      # eg: 98775,7192583653601,SCIPUB,BOS,8,140_day,2017-08-03 14:02:28.560950 => "SciPub,*,*"
+        test_string = "98775,7192583653601,SCIPUB,BOS,8,140_day,2017-08-03 14:02:28.560950"
+        self.assertIn(test_string, self.data_dictionary["SciPub,*,*"])
 
-    @unittest.skip("pending")
     def test_publisher_only_bucket_with_dash(self):
-        pass
-        # eg: => "McGraw-Hill,6,*"
+        test_string = "98795,9277080469051,MCGRAW-HILL,MSP,6,120_day,2017-04-02 11:05:31.561470"
+        self.assertIn(test_string, self.data_dictionary["McGraw-Hill,6,*"])
 
-    @unittest.skip("pending")
     def test_publisher_only_bucket_with_space(self):
-        pass
-        # eg: 99191,7848537371773,PENGUIN RANDOMHOUSE,MIA,4,30_day,2017-05-21 10:01:19.571428 => "Penguin Randomhouse,*,30_day"
+        test_string = "99191,7848537371773,PENGUIN RANDOMHOUSE,MIA,4,30_day,2017-05-21 10:01:19.571428"
+        self.assertIn(test_string, self.data_dictionary["Penguin Randomhouse,*,30_day"])
 
-    @unittest.skip("pending")
     def test_duration_only_bucket(self):
-        pass
-        # eg: 98819,9793386372887,PENGUIN RANDOMHOUSE,DTW,3,90_day,2017-07-14 14:06:01.562089 => "*,*,110_day"
+        test_string = "99999,9999999999999,MACMILLAN,MIA,3,110_day,2017-05-23 09:16:43.560846"
+        self.assertIn(test_string, self.data_dictionary["*,*,110_day"])
 
-    @unittest.skip("pending")
     def test_price_only_bucket(self):
-        pass
-        # made up eg: 99999,9999999999999,SCIPUB,MIA,3,110_day,2017-05-23 09:16:43.560846=> *,10,*"
+        test_string = "98809,8629055703626,OXFORD UNIVERSITY PRESS,DCA,10,70_day,2017-03-31 14:27:54.561831"
+        self.assertIn(test_string, self.data_dictionary["*,10,*"])
 
-    @unittest.skip("pending")
     def test_catch_all_bucket(self):
-        pass
-        # eg: 98765,0862728122370,OPENSTAX,CLT,5,150_day,2017-05-31 14:21:29.560404 => "*,*,*"
+        test_string = "98765,0862728122370,OPENSTAX,CLT,5,150_day,2017-05-31 14:21:29.560404"
+        self.assertIn(test_string, self.data_dictionary["*,*,*"])
 
     @unittest.skip("pending")
     def test_edge_case_repeated_bucket(self):
